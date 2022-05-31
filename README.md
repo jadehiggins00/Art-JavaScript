854c661e05c7b9989b928eddd3a141b70f0ee031
jadehiggins00 <jadehiggins2000@gmail.com>
2022-06-01 00:11:31 +0100

    added more images to readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Samples of art I have created with the help of books:
 
-## 29/05/2022 
+## 28/05/2022 
 ### rainbow spectrums playing with Hue and Saturation:
 ![rainbow](p5/book-examples/images/rainbow-spec2.png)
 
@@ -8,10 +8,30 @@
 
 ![rainbow](p5/book-examples/images/rainbow-spec4.png)
 
-## 30/05/2022
+## 29/05/2022
 ### A grid of colours with interpolation:
 ![rainbow](p5/book-examples/images/interpol-1.png)
 
 ![rainbow](p5/book-examples/images/interpol-2.png)
 
 ![rainbow](p5/book-examples/images/interpol-3.png)
+
+## 30/05/2022
+### Color Palletes from Image - Featuring various Towns from Sweden + Norway:
+#### Eskilstuna, Sweden
+![color-pal](p5/book-examples/images/eskil-1.png)
+### Distorted the Image with Hue & Saturation
+![color-pal](p5/book-examples/images/eskil-hue.png)
+![color-pal](p5/book-examples/images/eskil-sat.png)
+
+#### Narvik, Norway
+![color-pal](p5/book-examples/images/narvik.png)
+### Distorted the Image Saturation
+![color-pal](p5/book-examples/images/narvik-sat.png)
+
+#### Västerås, Sweden
+![color-pal](p5/book-examples/images/vast.jpg)
+### Distorted the Image with Hue & Saturation
+![color-pal](p5/book-examples/images/vast-hue.png)
+![color-pal](p5/book-examples/images/vast-saturation.png)
+
--- p5/book-examples/color-Image.js
+++ p5/book-examples/color-Image.js
@@ -10,7 +10,7 @@ the var called img
 */
 function preload(){
 
-    img = loadImage("pic1.jpg");
+    img = loadImage("images/narvik.jpg");
 
 }//end function
 
@@ -32,8 +32,49 @@ function draw(){
             // the pic is scanned line by line in the calculated grid spacing - rectSize
             var px = int(gridX * rectSize);
             var py = int(gridY * rectSize);
+            // the pixels are stored in the pixels[] array as a long  list of values.
+            // therefore from px and py, the corresponding i must be calculated
+            var i = (py * img.width + px)* 4;
+            var c = color(img.pixels[i], img.pixels[i+1], img.pixels[i+2], img.pixels[i+3]);
+            colors.push(c);
         }//end inner loop
 
     }//end outer for
 
+    // the colors are sorted using the sortColors(). this funciton must pass the colors and the sort mode
+    gd.sortColors(colors, sortMode);
+
+    var i=0;
+    for(var gridY = 0 ; gridY < tileCount; gridY++){
+        for(var gridX = 0 ; gridX < tileCount; gridX++){
+            // the grid is processed again to draw the pallette - the colors are taken from the array colors
+            fill(colors[i]);
+            rect(gridX*rectSize, gridY*rectSize, rectSize, rectSize);
+            i++;
+        }
+    }//end for
+
 }//end draw
+
+
+function keyReleased() {
+    // the ase.encode - allows the array of colors to be saved as an adobe swatch exchange file
+    // this pallette can then be loaded into adobe illustrator
+    if (key == 'c' || key == 'C') writeFile([gd.ase.encode(colors)], gd.timestamp(), 'ase');
+    if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  
+    if (key == '1') loadImage('data/pic1.jpg', setImage);
+    if (key == '2') loadImage('data/pic2.jpg', setImage);
+    if (key == '3') loadImage('data/pic3.jpg', setImage);
+    if (key == '4') loadImage('data/pic4.jpg', setImage);
+  
+    if (key == '5') sortMode = null;
+    if (key == '6') sortMode = gd.HUE;
+    if (key == '7') sortMode = gd.SATURATION;
+    if (key == '8') sortMode = gd.BRIGHTNESS;
+    if (key == '9') sortMode = gd.GRAYSCALE;
+  }//end fucntion
+  
+  function setImage(loadedImageFile) {
+    img = loadedImageFile;
+  }//end function
